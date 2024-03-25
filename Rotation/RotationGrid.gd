9aeb5dafe7849bd0dd44c7560ec03d2d3594d35a
Will <mardiros129@gmail.com>
2024-03-25 16:28:56 -0700

    Rotator updates
    Can rotate groups of tiles.
    
    Don't have to use magic number positions anymore, quadrants are set with a new tile layer.

--- Rotation/RotationGrid.gd
+++ Rotation/RotationGrid.gd
@@ -1,57 +1,88 @@
 extends TileMap
 
 
-@export var tile0_loc: Vector2
-@export var tile1_loc: Vector2
-@export var tile2_loc: Vector2
-@export var tile3_loc: Vector2
-@onready var tile_temp_loc
+@onready var quad_temp
 
-@onready var tile0_data
-@onready var tile1_data
-@onready var tile2_data
-@onready var tile3_data
+
+
+@onready var quadrant0: Array
+@onready var quadrant1: Array
+@onready var quadrant2: Array
+@onready var quadrant3: Array
 
 @onready var can_rotate = false
+@export var map_size = 10
 
 
-#func _ready():
-	
+func _ready():
+	# Check all cells in the grid. Magic number, change later.
+	for x in map_size:
+		for y in map_size:
+			var my_tile = get_cell_tile_data(1, Vector2i(x, y), false)
+			if my_tile != null:
+				var quadrant = my_tile.get_custom_data("Quadrant")
+				
+				# Add to the appropriate quadrant array.
+				if quadrant == 0:
+					quadrant0.append(Vector2i(x, y))
+				elif quadrant == 1:
+					quadrant1.append(Vector2i(x, y))
+				elif quadrant == 2:
+					quadrant2.append(Vector2i(x, y))
+				elif quadrant == 3:
+					quadrant3.append(Vector2i(x, y))
 
 func _process(delta):
 	if Input.is_action_just_pressed("rotate_key"):
 		if can_rotate:
-				rotate_group()
+				rotate_group_right()
 
-func rotate_group():
+func rotate_group_right():
+	var quad0_data: Array
+	var quad1_data: Array
+	var quad2_data: Array
+	var quad3_data: Array
+	
 	# Save tile data
-	tile0_data = get_cell_atlas_coords(0, tile0_loc, false)
-	tile1_data = get_cell_atlas_coords(0, tile1_loc, false)
-	tile2_data = get_cell_atlas_coords(0, tile2_loc, false)
-	tile3_data = get_cell_atlas_coords(0, tile3_loc, false)
+	for x in quadrant0.size():
+		quad0_data.append(get_cell_atlas_coords(0, quadrant0[x], false))
+	for x in quadrant1.size():
+		quad1_data.append(get_cell_atlas_coords(0, quadrant1[x], false))
+	for x in quadrant2.size():
+		quad2_data.append(get_cell_atlas_coords(0, quadrant2[x], false))
+	for x in quadrant3.size():
+		quad3_data.append(get_cell_atlas_coords(0, quadrant3[x], false))
 	
 	# Save tile location
-	tile_temp_loc = tile0_loc
-	tile0_loc = tile1_loc
-	tile1_loc = tile2_loc
-	tile2_loc = tile3_loc
-	tile3_loc = tile_temp_loc
+	quad_temp = quadrant0
+	quadrant0 = quadrant1
+	quadrant1 = quadrant2
+	quadrant2 = quadrant3
+	quadrant3 = quad_temp
 	
 	# Erase old cells
-	erase_cell(0, tile0_loc)
-	erase_cell(0, tile1_loc)
-	erase_cell(0, tile2_loc)
-	erase_cell(0, tile3_loc)
+	for x in quadrant0.size():
+		erase_cell(0, quadrant0[x])
+	for x in quadrant1.size():
+		erase_cell(0, quadrant1[x])
+	for x in quadrant2.size():
+		erase_cell(0, quadrant2[x])
+	for x in quadrant3.size():
+		erase_cell(0, quadrant3[x])
 	
 	# Draw new cells
-	set_cell(0, tile0_loc, 0, tile0_data, 0)
-	set_cell(0, tile1_loc, 0, tile1_data, 0)
-	set_cell(0, tile2_loc, 0, tile2_data, 0)
-	set_cell(0, tile3_loc, 0, tile3_data, 0)
+	for x in quadrant0.size():
+		set_cell(0, quadrant0[x], 0, quad0_data[x], 0)
+	for x in quadrant1.size():
+		set_cell(0, quadrant1[x], 0, quad1_data[x], 0)
+	for x in quadrant2.size():
+		set_cell(0, quadrant2[x], 0, quad2_data[x], 0)
+	for x in quadrant3.size():
+		set_cell(0, quadrant3[x], 0, quad3_data[x], 0)
 
 func _on_area_2d_area_entered(area):
 	can_rotate = true
-	print("hi")
+	print("Player entered.")
 
 func _on_area_2d_area_exited(area):
 	can_rotate = false
